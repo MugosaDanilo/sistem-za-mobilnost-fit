daa8cc83d4ce0e4b9a6e512ca2ef79933e0d78b6
Danilo Mugosa <129607223+MugosaDanilo@users.noreply.github.com>
2025-11-07 02:29:28 +0100

    instrukcije za instalaciju

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.10.0',
-        'version' => '12.10.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd43dc642b639f8a445afb87d2960cea7286ab5da',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -392,9 +392,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.10.0',
-            'version' => '12.10.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd43dc642b639f8a445afb87d2960cea7286ab5da',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
