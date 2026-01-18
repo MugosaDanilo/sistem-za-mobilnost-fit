6f692a65882c3308475c686b60e77444c2f84110
Danilo Mugosa <129607223+MugosaDanilo@users.noreply.github.com>
2026-01-18 14:29:13 +0100

    Info o prepisu update

---
+++ database/migrations/2026_01_18_141100_update_mapping_requests_status_enum.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+        if (config('database.default') === 'pgsql') {
+            DB::statement("ALTER TABLE mapping_requests DROP CONSTRAINT IF EXISTS mapping_requests_status_check");
+            DB::statement("ALTER TABLE mapping_requests ADD CONSTRAINT mapping_requests_status_check CHECK (status::text = ANY (ARRAY['pending'::text, 'accepted'::text, 'rejected'::text, 'completed'::text]))");
+        } else {
+            Schema::table('mapping_requests', function (Blueprint $table) {
+                $table->enum('status', ['pending', 'accepted', 'rejected', 'completed'])->default('pending')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (config('database.default') === 'pgsql') {
+            DB::statement("ALTER TABLE mapping_requests DROP CONSTRAINT IF EXISTS mapping_requests_status_check");
+            DB::statement("ALTER TABLE mapping_requests ADD CONSTRAINT mapping_requests_status_check CHECK (status::text = ANY (ARRAY['pending'::text, 'completed'::text]))");
+        } else {
+            Schema::table('mapping_requests', function (Blueprint $table) {
+                $table->enum('status', ['pending', 'completed'])->default('pending')->change();
+            });
+        }
+    }
+};
